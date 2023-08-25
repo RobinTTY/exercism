e47c812a60a51488ab1b6931d520af3a09509141
RobinTTY <muellerobin95@gmail.com>
2023-08-25 19:34:31 +0200

    Add exercise atbash-cipher

---
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -0,0 +1,18 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+export function encode(plainText: string): string {
+  return transcode(plainText).reduce(
+    (s, c, i) => (s += i === 0 || i % 5 ? c : " " + c),
+    ""
+  );
+}
+
+export function decode(cipherText: string): string {
+  return transcode(cipherText).join("");
+}
+
+function transcode(text: string): string[] {
+  return Array.from(text.replace(/\W/g, "").toLowerCase()).map((c) =>
+    c.match(/\d/) ? c : alphabet.charAt(25 - alphabet.indexOf(c))
+  );
+}
