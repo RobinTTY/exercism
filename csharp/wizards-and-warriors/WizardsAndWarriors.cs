415922825ca658f224347b7ad7b29745c737fb70
RobinTTY <muellerobin95@gmail.com>
2023-11-29 15:28:04 +0100

    Add WizardsAndWarriors exercise

---
+++ csharp/wizards-and-warriors/WizardsAndWarriors.cs
@@ -0,0 +1,60 @@
+using System;
+
+abstract class Character
+{
+    private string _characterType;
+    protected bool _vulnerable = false;
+
+    protected Character(string characterType)
+    {
+        _characterType = characterType;
+    }
+
+    public abstract int DamagePoints(Character target);
+
+    public virtual bool Vulnerable()
+    {
+        return _vulnerable;
+    }
+
+    public override string ToString()
+    {
+        return $"Character is a {_characterType}";
+    }
+}
+
+class Warrior : Character
+{
+    public Warrior() : base("Warrior")
+    {
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        return target.Vulnerable() ? 10 : 6;
+    }
+}
+
+class Wizard : Character
+{
+    private bool _spellPrepared = false;
+
+    public Wizard() : base("Wizard")
+    {
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        return _spellPrepared ? 12 : 3;
+    }
+
+    public void PrepareSpell()
+    {
+        _spellPrepared = true;
+    }
+
+    public override bool Vulnerable()
+    {
+        return _vulnerable || !_spellPrepared;
+    }
+}
